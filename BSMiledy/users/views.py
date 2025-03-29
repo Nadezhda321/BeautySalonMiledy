b02762b3c1ca5cabd27c696992a20fb9df0b17f2
Nadya <nadazuk92@gmail.com>
2025-03-29 21:10:10 +0300

    форма авторизации бэк

--- BSMiledy/users/views.py
+++ BSMiledy/users/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+
+
+
+@login_required #помечаем как требующую логина пользователя
 def profile(request):
     return render(request, 'users/profile.html')
